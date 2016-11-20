d8bb8a7df78818f0fde6a46d369df4aa2ac6cc6a
DAPPERstats <dapperstats@gmail.com>
2016-11-19 16:38:36 -0800

    working package

---
+++ R/test_function.R
@@ -0,0 +1,12 @@
+#' A welcoming function
+#'
+#' This function says hi.
+#' @param 
+#' @keywords cats
+#' @export
+#' @examples
+#' test_function()
+#
+
+test_function<-function()
+	print("Hello World")
